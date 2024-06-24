87e9fc20892d7cd3fa3fb76c86bcc3dabd9d762a
liamarfb <liamartafeb@gmail.com>
2024-06-24 14:43:11 +0800

    Upload Cekkhodam

---
+++ main.js
@@ -0,0 +1,28 @@
+function cekKhodam(event) {
+    event.preventDefault();
+    let nama = $('#search').val();
+    if (nama === '') {
+        alert('Nama Tidak Boleh Kosong!');
+    } else {
+        let status = ['isi', 'kosong'];
+        const randomStatus = Math.floor(Math.random() * status.length);
+        if (randomStatus == 0) {
+            let khodam = [
+                'Mio Karbu',
+                'Sumpit Gacoan',
+                'Keyboard Ompong',
+                'Tuyul Kebelet',
+                'Tusuk Sate'
+            ];
+            const random = Math.floor(Math.random() * khodam.length);
+            $('#result').html(`${nama.toUpperCase()} - Khodam Kamu (${khodam[random]})`);
+        } else {
+            $('#result').html(`${nama.toUpperCase()} - KOSONG`);
+        }
+        $('#search').val('');
+    }
+}
+
+$(document).ready(function() {
+    $('#btn-search').click(cekKhodam);
+});
